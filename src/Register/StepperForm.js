8bd54c6dd38b6780d9da58cccaaec44a11eb5943
Thiago Butignon <thiagobutignon@gmail.com>
2020-01-23 22:13:08 -0300

    Implementado o componente do StepperForm que controla o passo e a mudança de formulário

--- src/Register/StepperForm.js
+++ src/Register/StepperForm.js
@@ -39,4 +39,36 @@ export default function StepperForm() {
         return "Unknown stepIndex";
     }
   }
+
+  return (
+    <div>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map(label => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography>All steps completed</Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        ) : (
+          <div>
+            <div>{getStepContent(activeStep)}</div>
+            <div>
+              <Button disabled={activeStep === 0} onClick={handleBack}>
+                Back
+              </Button>
+              <Button variant="contained" color="primary" onClick={handleNext}>
+                {activeStep === steps.length - 1 ? "Finish" : "Next"}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
